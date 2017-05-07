3fd570bd3d96b0b1441c1916c8962f9be2db9c21
gotpop <liamsobczak@gmail.com>
2017-05-07 20:39:44 +0100

    Added bulma and tidy up

--- app/date.js
+++ app/date.js
@@ -21,13 +21,25 @@ export let dateSelect = {
   },
   buildWrap: function() {
     let createWrap = document.createElement('form')
-    createWrap.setAttribute('id', 'wrap')
+    createWrap.setAttribute('class', 'columns')
 
     for (let i = 0; i < 3; i++) {
+
       let adjustedIndex = i + 1
+      let createDiv = document.createElement('div')
+      let createP = document.createElement('p')
+      let createSpan = document.createElement('span')
       let createSelect = document.createElement('select')
+
+      createDiv.setAttribute('class', 'field')
+      createP.setAttribute('class', 'control column')
+      createSpan.setAttribute('class', 'select')
       createSelect.setAttribute('id', 'selectID' + adjustedIndex)
-      createWrap.appendChild(createSelect)
+
+      createDiv.appendChild(createP)
+      createP.appendChild(createSpan)
+      createSpan.appendChild(createSelect)
+      createWrap.appendChild(createP)
       dateSelect.addListener(createSelect)
     }
     document.body.appendChild(createWrap)
