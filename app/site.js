a2dac9003e3597e5b9f41fe5cc0f11947e1a2b5a
Liam Got Pop <liam@gotpop.net>
2017-05-17 21:23:24 +0100

    Add basic animation to test

--- app/site.js
+++ app/site.js
@@ -1,32 +1,15 @@
 var earth = new Image();
 let winWidth = window.innerWidth;
 let winHeight = window.innerHeight;
-
-
+let myCanvas = document.createElement("canvas");
+myCanvas.width = winWidth;
+myCanvas.height = winHeight;
+myCanvas.setAttribute('id', 'canvas')
+document.body.appendChild(myCanvas);
+earth.src =
+"http://upload.wikimedia.org/wikipedia/commons/d/d2/Svg_example_square.svg";
 
 function init() {
-
-    let myCanvas = document.createElement("canvas");
-
-    myCanvas.width = winWidth;
-    myCanvas.height = winHeight;
-    myCanvas.setAttribute('id', 'canvas')
-
-    var ctx = myCanvas.getContext("2d");
-
-    var img = new Image();
-    img.onload = function() {
-        ctx.drawImage(img, 200, 20).rotate(((2 * Math.PI) / 2) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds())
-
-    };
-    img.src =
-        "http://upload.wikimedia.org/wikipedia/commons/d/d2/Svg_example_square.svg";
-
-    document.body.appendChild(myCanvas);
-
-
-    earth.src =
-        "http://upload.wikimedia.org/wikipedia/commons/d/d2/Svg_example_square.svg";
     window.requestAnimationFrame(draw);
 }
 
@@ -36,13 +19,13 @@ function draw() {
 
   ctx.clearRect(0, 0, winWidth, winHeight); // clear canvas
   ctx.save();
-  ctx.translate(150, 150);
+  ctx.translate(200, 200);
 
   // Earth
   var time = new Date();
  	ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-  ctx.translate(105, 0);
-  ctx.drawImage(earth, -12, -12);
+  // ctx.translate(105, 0);
+  ctx.drawImage(earth, 50, 50);
   ctx.restore();
   window.requestAnimationFrame(draw);
 }
