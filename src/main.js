e1a20ee7c97eb2defa41f0be64424a29deb866a1
gotpop <liamsobczak@gmail.com>
2017-06-25 17:39:09 +0100

    'Base'

--- src/main.js
+++ src/main.js
@@ -11,7 +11,15 @@ import './css/index.scss'
 
 import DateFilter from './index.js'
 
-let view = document.getElementById('view')
 
-// let makeDateFilter = new DateFilter
-// view.appendChild(makeDateFilter)
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log('loaded');
+  let view = document.getElementById('view')
+
+  let makeDateFilter = new DateFilter().renderField()
+
+  console.log('makeDateFilter: ', makeDateFilter)
+  console.log('view: ', view)
+  console.log("DOM fully loaded and parsed");
+  view.appendChild(makeDateFilter)
+});
