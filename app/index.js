8039a05e17d73a3bb85a62c420739e828031a4e7
gotpop <liamsobczak@gmail.com>
2017-06-25 14:36:23 +0100

    'Base'

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 // Require static files
 require('file-loader?name=[name].[ext]!./index.html');
-require('file-loader?name=[name].[ext]!./favicon.ico');
-require('file-loader?name=[name].[ext]!./site.css');
+require('file-loader?name=[name].[ext]!./img/favicon.ico');
+require('file-loader?name=[name].[ext]!./css/site.css');
 
 // // Import JS
 import './date.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,67 @@
-var path = require('path');
+let path = require('path')
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+let CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '')
-  },
-  devServer: {
-    contentBase: path.join(__dirname, ""),
-    compress: true,
-    port: 9000,
-    watchContentBase: true
-  }
-};
+    entry: __dirname + '/app/index.js',
+    output: {
+        filename: 'app.bundle.js',
+        path: __dirname + '/dist'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "/dist/"),
+        port: 9000,
+        watchContentBase: true
+    },
+    module: {
+        rules: [
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/,
+              use: [
+                'file-loader'
+              ]
+            },
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['env']
+                }
+              }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        interpolate: true
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.svg/,
+                use: {
+                    loader: 'svg-url-loader',
+                    options: {
+                    noquotes: true
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            favicon: "./app/img/favicon.ico",
+            title: 'GotPop',
+            template: './app/my-index.ejs',
+            inject: 'head'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+}
