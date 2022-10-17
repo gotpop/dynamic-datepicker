0912bd2132f2f65369cc6012402450e21583df67
gotpop <liam@gotpop.net>
2022-10-17 20:11:22 +0100

    Updating build

--- src/main.js
+++ src/main.js
@@ -19,5 +19,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let view = document.getElementById('view')
   let makeDateFilter = new DateFilter().renderField()
   view.appendChild(makeDateFilter)
+  console.log('view :', view);
 
 });
